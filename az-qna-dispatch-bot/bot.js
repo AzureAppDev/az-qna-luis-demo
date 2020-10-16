5a6d0958294c4969d151436bc2196937bfcb46fa
Paul Riviera <PaulRiviera@outlook.com>
2020-10-16 15:43:10 -0500

    update the bot

--- az-qna-dispatch-bot/bot.js
+++ az-qna-dispatch-bot/bot.js
@@ -1,12 +1,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// Step 1: Where is the HR Portal?
+
+// Step 2: Ask another question that luis will respond with an intent
+    // use the intent to pull Data from Table Storage
+
+
 const { ActivityHandler, MessageFactory } = require('botbuilder');
-const { QnAMaker } = require('botbuilder-ai');
+const { LuisRecognizer, QnAMaker } = require('botbuilder-ai');
 
 class EchoBot extends ActivityHandler {
     constructor() {
         super();
+
+        const dispatchRecognizer = new LuisRecognizer({
+            applicationId: process.env.LuisAppId,
+            endpointKey: process.env.LuisAPIKey,
+            endpoint: `https://${ process.env.LuisAPIHostName }.api.cognitive.microsoft.com`
+        }, {
+            includeAllIntents: true,
+            includeInstanceData: true
+        }, true);
 
         const qnaMaker = new QnAMaker({
             knowledgeBaseId: process.env.QnAKnowledgebaseId,
@@ -14,16 +29,34 @@ class EchoBot extends ActivityHandler {
             host: process.env.QnAEndpointHostName
         });
 
+        this.dispatchRecognizer = dispatchRecognizer;
+        this.qnaMaker = qnaMaker;
+
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
 
-            const response = await qnaMaker.getAnswers(context)
-            const replyText = `QnA Answer ${ response[0].score }: ${ response[0].answer }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            // Call QnA and see if a result is returned
+            //const qnaResult = await qnaMaker.getAnswers(context);
+
+            // First, we use the dispatch model to determine which cognitive service (LUIS or QnA) to use.
+            const recognizerResult = await dispatchRecognizer.recognize(context);
+
+            console.log(recognizerResult);
+
+            // Top intent tell us which cognitive service to use.
+            const intent = LuisRecognizer.topIntent(recognizerResult);
+
+            // Next, we call the dispatcher with the top intent.
+            await this.dispatchToTopIntentAsync(context, intent, recognizerResult);
+
+            // const response = await qnaMaker.getAnswers(context)
+            // const replyText = `QnA Answer ${ response[0].score }: ${ response[0].answer }`;
+            // await context.sendActivity(MessageFactory.text(replyText, replyText));
 
             // const replyText = `QnA Answer ${ response[0].score }: ${ response[0].answer }`;
             // await context.sendActivity(MessageFactory.text(replyText, replyText));
             // By calling next() you ensure that the next BotHandler is run.
+
             await next();
         });
 
@@ -38,6 +71,18 @@ class EchoBot extends ActivityHandler {
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+    }
+
+    async dispatchToTopIntentAsync(context, intent, recognizerResult) {
+        switch (intent) {
+        case 'none':
+            await this.processHomeAutomation(context, recognizerResult.luisResult);
+            break;
+        default:
+            console.log(`Dispatch unrecognized intent: ${ intent }.`);
+            await context.sendActivity(`Dispatch unrecognized intent: ${ intent }.`);
+            break;
+        }
     }
 }
 
