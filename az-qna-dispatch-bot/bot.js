0ad4680ecec7a698087d870cfe81027b769ac362
Paul Riviera <paulriviera@outlook.com>
2020-10-22 21:28:52 -0500

    clean the bot.js file for development

--- az-qna-dispatch-bot/bot.js
+++ az-qna-dispatch-bot/bot.js
@@ -6,7 +6,6 @@
 // Step 2: Ask another question that luis will respond with an intent
     // use the intent to pull Data from Table Storage
 
-
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 const { LuisRecognizer, QnAMaker } = require('botbuilder-ai');
 
@@ -14,50 +13,11 @@ class EchoBot extends ActivityHandler {
     constructor() {
         super();
 
-        const dispatchRecognizer = new LuisRecognizer({
-            applicationId: process.env.LuisAppId,
-            endpointKey: process.env.LuisAPIKey,
-            endpoint: `https://${ process.env.LuisAPIHostName }.api.cognitive.microsoft.com`
-        }, {
-            includeAllIntents: true,
-            includeInstanceData: true
-        }, true);
-
-        const qnaMaker = new QnAMaker({
-            knowledgeBaseId: process.env.QnAKnowledgebaseId,
-            endpointKey: process.env.QnAEndpointKey,
-            host: process.env.QnAEndpointHostName
-        });
-
-        this.dispatchRecognizer = dispatchRecognizer;
-        this.qnaMaker = qnaMaker;
-
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
 
-            // Call QnA and see if a result is returned
-            //const qnaResult = await qnaMaker.getAnswers(context);
-
-            // First, we use the dispatch model to determine which cognitive service (LUIS or QnA) to use.
-            const recognizerResult = await dispatchRecognizer.recognize(context);
-
-            console.log(recognizerResult);
-
-            // Top intent tell us which cognitive service to use.
-            const intent = LuisRecognizer.topIntent(recognizerResult);
-
-            // Next, we call the dispatcher with the top intent.
-            await this.dispatchToTopIntentAsync(context, intent, recognizerResult);
-
-            // const response = await qnaMaker.getAnswers(context)
-            // const replyText = `QnA Answer ${ response[0].score }: ${ response[0].answer }`;
-            // await context.sendActivity(MessageFactory.text(replyText, replyText));
-
-            // const replyText = `QnA Answer ${ response[0].score }: ${ response[0].answer }`;
-            // await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
-
+            await context.sendActivity(`ECHO: ${context.activity.text}`);
             await next();
+            
         });
 
         this.onMembersAdded(async (context, next) => {
